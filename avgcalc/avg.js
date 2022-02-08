cee9407691f1f6e2729eb53fa26079bd6816a70b
Saboor <42164502+saboooor@users.noreply.github.com>
2022-02-08 11:13:12 -0700

    finish avg calc

--- avgcalc/avg.js
+++ avgcalc/avg.js
@@ -1,9 +1,21 @@
 // Update the average number
 function updateAvg() {
     // Get the values
-    const num1 = document.getElementById('num1').value;
-    const num2 = document.getElementById('num2').value;
-    const num3 = document.getElementById('num3').value;
+    const input = document.getElementById('input').value;
+    const array = input.split(',');
+
+    // Add numbers together
+    let addednums = 0;
+    array.forEach(number => {
+        addednums = addednums + parseInt(number);
+    });
+    
+    // calculate the average
+    const avg = addednums / array.length;
+
+    // output the average
+    if (!isNaN(avg)) document.getElementById('output').innerHTML = `The average of the ${array.length} numbers is: ${avg}`;
+    else document.getElementById('output').innerHTML = `Please enter valid numbers separated by commas.`;
 }
 
 // Call the function on launch
