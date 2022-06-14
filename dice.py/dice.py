507191ee7a7ebd5918de56f8debfa9f9ecae85de
saboooor <saboor547@gmail.com>
2022-06-14 10:57:08 -0600

    working dice.py

--- dice.py/dice.py
+++ dice.py/dice.py
@@ -1,17 +1,33 @@
 from js import alert
 import random
 
-def roll(event):
-  console.log(event.srcElement.id)
-  amount = 1
-  if event.srcElement.id == 'five': amount = 5
-  if event.srcElement.id == 'custom': amount = int(Element('amount').value)
+def roll_loop(amount):
   result = []
   for x in range(amount):
     dice1 = random.randint(1, 6);
     dice2 = random.randint(1, 6);
     result.append(f'{dice1}, {dice2} (sum: {dice1 + dice2})')
-  alert('\n'.join(result))
-  return
+  Element('result').element.innerText = '\n'.join(result)
 
-def customroll():
+def roll(event):
+  amount = 1
+  if event.srcElement.id == 'five': amount = 5
+  roll_loop(amount)
+
+def customroll(event):
+  amount = int(Element('amount').element.value)
+  if not amount.isdigit():
+    alert('Please enter a valid number.')
+    return
+  roll_loop(amount)
+
+def snake_eyes(event):
+  result = []
+  while True:
+    dice1 = random.randint(1, 6);
+    dice2 = random.randint(1, 6);
+    result.append(f'{dice1}, {dice2} (sum: {dice1 + dice2})')
+    if dice1 == 1 and dice2 == 1:
+      result.append(f'Snake eyes! It took {len(result)} rolls to get Snake eyes!')
+      Element('result').element.innerText = '\n'.join(result)
+      return
